e886583b724bad59ad29cc88d065aca72ed72fc8
LuisaFC <luisabsb@gmail.com>
2024-09-06 15:40:13 -0300

    use cases

---
+++ api/src/routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express'
+
+export const router = Router();
+
+//List Destination
+
+//Create Destination by country/city/name/sttate
+
+//Create Destination
+
+//List Route
+
+//List Route by name
+
+//Create Route
+
+//delete route
+
+//create
+
+//List fav destinations
+
+//List fav routes
+
+//delete user
