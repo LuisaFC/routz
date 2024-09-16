39f903ee1829535de5f2cd1508061a208427dc25
LuisaFC <luisabsb@gmail.com>
2024-09-15 21:06:48 -0300

    fix: correct creat routes

--- api/src/app/useCases/Route/createRoute.ts
+++ api/src/app/useCases/Route/createRoute.ts
@@ -3,9 +3,9 @@ import { Route } from '../../models/Route'
 
 export async function createRoute(req: Request, res: Response){
   try {
-    const {name, destination, user} = req.body;
+    const {name, destinations, user} = req.body;
 
-    const route = await Route.create({name, destination, user})
+    const route = await Route.create({name, destinations, user})
 
     res.status(201).json(route)
 
